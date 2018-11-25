1db62588bd15579f949389a9e2d169663bd833af
perry <User@Tipzee-Aspire.(none)>
2018-11-25 16:05:24 +0800

    Db config

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes/routes');
+const db = require('./db');
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-routes(app);
+
+routes(app, db);
 
 app.listen(3001,() => {
     console.log('entered');
