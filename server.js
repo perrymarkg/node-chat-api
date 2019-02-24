df4d6f0a36f30237ad07423953ce2410b5234422
perry <User@Tipzee-Aspire.(none)>
2019-02-24 17:46:52 +0800

    Set index

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 const routes = require('./routes/auth.route');
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/chat', {useNewUrlParser: true});
 mongoose.connection.on('error', (r) => {
     console.log(r);
