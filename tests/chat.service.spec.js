ebf201b0e89ff5c16f4127d6b6871c8c442a5782
perry <User@Tipzee-Aspire.(none)>
2019-03-24 14:37:28 +0800

    Refactor tests

--- tests/chat.service.spec.js
+++ tests/chat.service.spec.js
@@ -14,19 +14,7 @@ describe("Chat Service tests", () => {
     beforeAll(async (done) => {
         
         await idb.init();
-        User1 = await new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: 'mail1@google.com',
-            username: 'dumm1',
-            password: 'samplepassword'
-        }).save();
-
-        User2 = await new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: 'mail2@google.com',
-            username: 'dummy2',
-            password: 'samplepassword'
-        }).save();
+        
         done();
     });
 
@@ -35,6 +23,20 @@ describe("Chat Service tests", () => {
     });
 
     beforeEach(async() => {
+
+        User1 = await new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: 'mail1@google.com',
+            username: 'dumm1',
+            password: 'samplepassword'
+        }).save()
+
+        User2 = await new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: 'mail2@google.com',
+            username: 'dummy2',
+            password: 'samplepassword'
+        }).save();
 
         sampleConvo = new Convo({
             _id: new mongoose.Types.ObjectId(),
@@ -46,70 +48,81 @@ describe("Chat Service tests", () => {
 
     afterEach(async() => {
         await idb.dropAll();
-    })
+    });
 
-    it("Should create and save 1 to 1 conversation", () => {
+    it("Should have created users for testing", async() => {
 
-        const convo = ChatService
-            .createConversation(User1, User2)
+        const users = await User.find({}).exec()
+        expect(users.length).toBe(2);
+    });
+
+    it("Should create and save 1 to 1 conversation", async() => {
+
+        const convo = await ChatService.saveConversation(User1, User2)
         
         const expectedConvo = new Convo({
             _id: new mongoose.Types.ObjectId(),
             members: [User1._id, User2._id],
             createdBy: User1._id
         });
-        //console.log(expectedConvo);
-        //console.log(convo.members, expectedConvo.members);
+        
         expect(convo instanceof Convo).toBe(true);
-        expect(convo.members[0]).toEqual(expectedConvo.members[0]);
-        expect(convo.members[1]).toEqual(expectedConvo.members[1]);
-        expect(convo.createdBy).toEqual(expectedConvo.createdBy);
-
-       ChatService
-            .saveConversation(User1, User2)
-            .then(result => {
-                expect(result instanceof Convo).toBe(true);
-                expect(result.members[0]).toEqual(expectedConvo.members[0]);
-                expect(result.members[1]).toEqual(expectedConvo.members[1]);
-                expect(result.createdBy).toEqual(expectedConvo.createdBy);
-            });
-
+        expect(JSON.stringify(convo.members)).toEqual(JSON.stringify(expectedConvo.members));
+        expect(convo.createdBy).toBe(User1._id);
     });
 
     it("Should retrieve user conversations", async() => {
-        const result = await ChatService
-            .getUserConversations(User1, User2)
-            .then(conversations => conversations)
-            .catch(error => error);
-
+        await sampleConvo.save();
+        const result = await ChatService.getUserConversations(User1, User2);
         expect(result.length).toBe(1);
+        expect(result[0].members.length).toBe(2);
+        expect(result[0].members[0] instanceof User).toBe(true);
     });
 
     it("Should not save conversation for one member", async() => {
-        const  result = await ChatService
-            .saveConversation(User1)
-            .then(result => result)
-            .catch(error => error);
-
+        let result;
+        try {
+            result = await ChatService.saveConversation(User1)
+        } catch (e) {
+            result = e;
+        }
         expect(result instanceof Error).toBe(true);
     });
 
-    it("should retrive a conversation", async() => {
+    it("should retrieve a conversation", async() => {
 
-        let id = new mongoose.Types.ObjectId();
-        const test = await new Convo({
-            _id: id,
-            members: [User1._id, User2._id],
-            createdBy: User1._id
-        }).save().then(result => result);
-
-        const result = await ChatService
-            .getConverSation(id)
-            .then(conversation => conversation)
-            .catch(error => error)
+        const convo = await sampleConvo.save();
+        const result = await ChatService.getConverSation(convo._id)
         
         expect(result instanceof Convo).toBe(true);
-        expect(result._id).toEqual(id);
+        expect(result._id).toEqual(convo._id);
+        expect(result.createdBy instanceof User).toBe(true);
     });
+
+    it("should add a conversation", async() => {
+
+        await sampleConvo.save();
+        const message = 'Hello there';
+        const msg = await ChatService.addMessage(sampleConvo.id, User1, message)
+
+        expect(msg instanceof Message).toBe(true);
+        expect(msg.message).toEqual(message);
+        expect(msg.createdBy).toEqual(User1._id);
+
+    });
+
+    it("it should retrive a conversation", async() => {
+
+        const convo = await sampleConvo.save()
+
+        const message = 'Hello there';
+        const msg = await ChatService.addMessage(convo.id, User1, message)
+
+        const msg_result = await ChatService.getMessage(msg._id);
+        console.log(msg_result);
+        expect(msg_result instanceof Message).toBe(true);
+        expect(msg_result.createdBy instanceof User).toBe(true)
+       
+    })
 
 });
