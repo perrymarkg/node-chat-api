d862728b77536d0efbe4e77e44b2201aec9a6cb4
perry <User@Tipzee-Aspire.(none)>
2019-03-24 14:36:23 +0800

    Added timestamp, corrected schema

--- db/models/message.model.js
+++ db/models/message.model.js
@@ -1,11 +1,10 @@
-// Convesation Model
 const mongoose = require('mongoose');
 
 const MessageSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     convoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Convo' },
-    members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true }],
-    creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
-});
+    message: {type: String},
+    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+}, {timestamps: true});
 
 module.exports = mongoose.model('Message', MessageSchema);
