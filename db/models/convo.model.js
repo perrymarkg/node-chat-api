a2a35d96ae74f784b8a12dccaa6ac4240a4228b3
perry <User@Tipzee-Aspire.(none)>
2019-03-24 14:35:38 +0800

    added timestamps

--- db/models/convo.model.js
+++ db/models/convo.model.js
@@ -1,10 +1,9 @@
-// Convesation Model
 const mongoose = require('mongoose');
 
 const ConvoSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    createdBy: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'}
-});
+    createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+}, {timestamps: true});
 
 module.exports = mongoose.model('Convo', ConvoSchema);
