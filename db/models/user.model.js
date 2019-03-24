e14c1639051dfa332dd1fcd24716430ce8a2be50
perry <User@Tipzee-Aspire.(none)>
2019-03-24 14:36:43 +0800

    added timestamp

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -6,7 +6,7 @@ const UserSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true },
     email: {type: String, required: true, unique: true },
     password: {type: String, required: true}
-});
+}, {timestamps: true});
 
 UserSchema.pre('save', function(next) {
     var User = this;
